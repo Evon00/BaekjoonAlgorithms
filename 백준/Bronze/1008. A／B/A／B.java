d0c58ad2fbcf43dad581426435e75658b6a99430
Evon00 <123055298+Evon00@users.noreply.github.com>
2023-12-27 15:33:00 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        double result = a / (b * 1.0);
+        System.out.println(result);
+    }
+}
