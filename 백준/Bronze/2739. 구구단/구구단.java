2a436f2ea5b52c44dcb0898cdcbfd3c096a4fddc
Evon00 <123055298+Evon00@users.noreply.github.com>
2023-12-28 15:27:37 +0900

    [Bronze V] Title: 구구단, Time: 248 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for(int i = 1; i <= 9; i++){
+            System.out.println(number + " * " + i +" = "+(number*i));
+        }
+    }
+}
