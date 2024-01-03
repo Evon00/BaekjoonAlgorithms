b4a8295cf5a7f44bcfb5d62bde9ad74e2f898e72
Evon00 <123055298+Evon00@users.noreply.github.com>
2024-01-03 16:36:32 +0900

    [Bronze II] Title: 공 바꾸기, Time: 144 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[N];
+
+        for(int i = 0; i < N; i++){
+            basket[i] = i+1;
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int first = Integer.parseInt(st.nextToken())-1;
+            int second = Integer.parseInt(st.nextToken())-1;
+
+            int change = 0;
+
+            change = basket[first];
+            basket[first] = basket[second];
+            basket[second] = change;
+            
+        }
+
+        for(int i = 0; i < N; i++){
+            bw.write(basket[i]+" ");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+        
+    }
+}
