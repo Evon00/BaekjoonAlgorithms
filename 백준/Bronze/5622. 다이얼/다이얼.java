90b256bc0b7cf8a1640ca476c64f07758a938e83
Evon00 <123055298+Evon00@users.noreply.github.com>
2024-01-04 16:12:34 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+        int totalTime = 0;
+
+        for(char c : word.toCharArray()){
+            int number = getDialNumber(c);
+            totalTime += number + 1;
+        }
+        bw.write(String.valueOf(totalTime));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    private static int getDialNumber(char c){
+        switch(c) {
+            case 'A':
+            case 'B':
+            case 'C':
+                return 2;
+            case 'D':
+            case 'E':
+            case 'F':
+                return 3;
+            case 'G':
+            case 'H':
+            case 'I':
+                return 4;
+            case 'J':
+            case 'K':
+            case 'L':
+                return 5;
+            case 'M':
+            case 'N':
+            case 'O':
+                return 6;
+            case 'P':
+            case 'Q':
+            case 'R':
+            case 'S':
+                return 7;
+            case 'T':
+            case 'U':
+            case 'V':
+                return 8;
+            case 'W':
+            case 'X':
+            case 'Y':
+            case 'Z':
+                return 9;
+            default:
+                return 0;
+        }
+    }
+}
