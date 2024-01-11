4f1c44863fb930336cafae82882ce3b87925300d
유재혁 (Yu JaeHyeok) <123055298+Evon00@users.noreply.github.com>
2024-01-11 16:51:00 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Long n = Long.parseLong(br.readLine());
+        bw.write(String.valueOf(n*n)+"\n");
+        bw.write("2");
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
