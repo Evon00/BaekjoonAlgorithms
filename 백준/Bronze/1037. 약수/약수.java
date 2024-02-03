7f0868337f2378ee6e4ff0baee3abafa56c5d47b
유재혁 (Yu JaeHyeok) <123055298+Evon00@users.noreply.github.com>
2024-02-03 13:29:47 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N =  Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int[] arr = new int[N];
+        
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        bw.write(String.valueOf(arr[0]*arr[N-1]));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
