10efdf3735105955baeb46528ad4cff3da7a866b
유재혁 (Yu JaeHyeok) <123055298+Evon00@users.noreply.github.com>
2025-03-11 17:15:00 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if(n % 2 == 0) System.out.println("CY");
+        else System.out.println("SK");
+ 
+    }
+}
